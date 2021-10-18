17585f1b7dee65dad7ffad84d5b3c11c09e3f093
Celeste Farris <87283933+cMfarris@users.noreply.github.com>
2021-10-18 00:15:54 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,20 @@
+# Note Taker Starter Code
+
+OBJECTIVE
+
+This application is to be used to write and save notes. You will use a JSON file to grab new data from the file.
+
+deployed app:
+github page:
+
+NOTE TAKER; EXPRESS.JS
+
+This allows users to use the note tracker to keep up with information and go to when it is needed. It will help same time if you forget or need to write down something important.
+
+TECHNOLOGIES USED:
+- JavaScript 
+- Express.js
+- mySQL
+
+
+
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const allNotes = JSON.parse(fs.readFileSync("./db/db.json, utf8"));
+
+//notes.html 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+// index.html 
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+// db.json file
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+
+    readTextFile('Develop/db/db.json', function (text) {
+        const dbData = db.json(text);
+        console.log(dbData);
+    });
+});
+//listener
+app.listen(PORT, () => console.log(3001));
